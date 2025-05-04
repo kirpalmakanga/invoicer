3ed441c4dcf357f7318b9e1e93b964745684d684
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-04 15:03:54 +0200

    utils: added isEqual

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,29 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function isEqual(a: any, b: any): boolean {
+    if (a === b) return true;
+
+    if (a == null || b == null) return a === b;
+
+    if (typeof a !== typeof b) return false;
+
+    if (Array.isArray(a) && Array.isArray(b)) {
+        if (a.length !== b.length) return false;
+        return a.every((item, index) => isEqual(item, b[index]));
+    }
+
+    if (typeof a === 'object') {
+        const keysA = Object.keys(a);
+        const keysB = Object.keys(b);
+
+        if (keysA.length !== keysB.length) return false;
+        return keysA.every((key) => key in b && isEqual(a[key], b[key]));
+    }
+
+    return false;
 }
