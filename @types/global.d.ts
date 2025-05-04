f791663d156a95807ce563f08843d5f1d7a9fe3d
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-04 18:23:13 +0200

    updated global types

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -7,20 +7,22 @@ interface Customer {
 }
 
 interface InvoiceItem {
-    productId: string;
     name: string;
     amount: number;
     pricePerUnit: number;
     unit: 'hour' | 'day';
 }
 
+type InvoiceStatus = 'pending' | 'unpaid' | 'paid';
+type PaymentMethod = 'Bank transfer' | 'Credit card' | 'PayPal';
+
 interface Invoice {
     id: string;
     invoiceId: string;
     customerId: string;
     items: InvoiceItem[];
-    paymentMethod: 'Bank transfer' | 'Credit card' | 'PayPal';
-    status: 'pending' | 'unpaid' | 'paid';
+    paymentMethod: PaymentMethod;
+    status: InvoiceStatus;
     dateCreated: number;
     dateSent: number;
 }
