9b627c054f2bd4108a6874b7a8ac2ede1fb0b7a5
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-05 03:37:36 +0200

    pages/customer: bound template to store

--- app/customer/[customerId]/page.tsx
+++ app/customer/[customerId]/page.tsx
@@ -1,37 +1,61 @@
 'use client';
 
-import { useState } from 'react';
+import { useMemo } from 'react';
+import { Mail, Phone, MapPin } from 'lucide-react';
 import { useParams } from 'next/navigation';
-import { mockInvoices } from '@/mocks/invoices';
 import { DataTable } from '@/components/data-table';
 import { customerColumns } from '@/components/invoices/columns';
 import H1 from '@/components/atoms/H1';
 import H2 from '@/components/atoms/H2';
+import { NotFound } from '@/components/NotFound';
+import { useInvoicesStore } from '@/store/invoices';
+import { useCustomersStore } from '@/store/customers';
 
 export default function Customer() {
     const { customerId } = useParams<{ customerId: string }>();
-    const [invoices] = useState<Invoice[]>(mockInvoices);
+    const customers = useCustomersStore(({ customers }) => customers);
+    const invoices = useInvoicesStore(({ invoices }) => invoices);
 
-    return (
+    const customer = useMemo(
+        () => customers.find(({ id }) => id === customerId),
+        [customers]
+    );
+
+    const customerInvoices = useMemo(
+        () =>
+            invoices.filter(
+                ({ customerId: itemCustomerId }) =>
+                    itemCustomerId === customerId
+            ),
+        [invoices]
+    );
+
+    return customer ? (
         <>
-            <H1 className="mb-2">Customer {customerId}</H1>
+            <H1 className="mb-4">{customer.name}</H1>
 
-            <div className="flex flex-col gap-1">
-                <p>Address: 1 rue Jean Dubois</p>
-                <p>Postcode: 87000</p>
-                <p>City: Limoges</p>
-                <p>Country: France</p>
+            <div className="flex flex-col gap-4">
+                <p className="flex items-center gap-2">
+                    <MapPin />
+                    <span>{customer.address}</span>
+                </p>
+
+                <p className="flex items-center gap-2">
+                    <Phone />
+                    <a href={`tel:${customer.tel}`}>{customer.tel}</a>
+                </p>
+
+                <p className="flex items-center gap-2">
+                    <Mail />
+                    <a href={`mailto:${customer.email}`}>{customer.email}</a>
+                </p>
             </div>
 
             <H2 className="mt-8 mb-2">Invoice history</H2>
 
-            <DataTable
-                columns={customerColumns}
-                data={invoices.filter(
-                    ({ customerId: itemCustomerId }) =>
-                        itemCustomerId === customerId
-                )}
-            />
+            <DataTable columns={customerColumns} data={customerInvoices} />
         </>
+    ) : (
+        <NotFound />
     );
 }
