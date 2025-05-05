05a32fc536678022e88d9a26c188645948f39fb3
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-05 02:50:33 +0200

    added component: NotFound

---
+++ components/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+    return (
+        <div className="flex flex-col grow items-center justify-center">
+            <p className="text-4xl">404</p>
+            <p>Content not found.</p>
+        </div>
+    );
+}
