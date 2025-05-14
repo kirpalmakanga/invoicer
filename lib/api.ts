209ee55a47da6fdd6e80864ccf45f9b93e47565a
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-14 18:52:26 +0200

    lib/api: fixed response destructuring

--- lib/api.ts
+++ lib/api.ts
@@ -1,24 +1,30 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: `https://api.invoicer.dev`,
 });
 
 /** Customers */
 export async function getAllCustomers() {
-    const { data } = await api.get('/customers');
+    const {
+        data: { data },
+    } = await api.get('/customers');
 
     return data as Customer[];
 }
 
 export async function getCustomerById(id: string) {
-    const { data } = await api.get(`/customers/${id}`);
+    const {
+        data: { data },
+    } = await api.get(`/customers/${id}`);
 
     return data as Customer;
 }
 
 export async function createCustomer(customerData: CustomerFormData) {
-    const { data } = await api.post(`/customers`, customerData);
+    const {
+        data: { data },
+    } = await api.post(`/customers`, customerData);
 
     return data as Customer;
 }
@@ -36,19 +42,25 @@ export async function deleteCustomer(id: string) {
 
 /** Invoices */
 export async function getAllInvoices() {
-    const { data } = await api.get('/invoices');
+    const {
+        data: { data },
+    } = await api.get('/invoices');
 
     return data as Invoice[];
 }
 
 export async function getInvoiceById(id: string) {
-    const { data } = await api.get(`/invoices/${id}`);
+    const {
+        data: { data },
+    } = await api.get(`/invoices/${id}`);
 
     return data as Invoice;
 }
 
 export async function createInvoice(invoiceData: InvoiceFormData) {
-    const { data } = await api.post(`/invoices`, invoiceData);
+    const {
+        data: { data },
+    } = await api.post(`/invoices`, invoiceData);
 
     return data as Invoice;
 }
