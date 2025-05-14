887bab1a8a771457959e5a8fb9edb10d07e1f648
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-14 02:57:04 +0200

    store/invoices: added collection fetch

--- store/invoices.ts
+++ store/invoices.ts
@@ -1,17 +1,24 @@
 import { create } from 'zustand';
-import { mockInvoices } from '@/mocks';
+// import { mockInvoices } from '@/mocks';
+import { getAllInvoices } from '@/lib/api';
 
 interface InvoicesState {
     invoices: Invoice[];
-    addInvoice: (invoice: Invoice) => void;
+    fetchInvoices: () => void;
+    addInvoice: (invoicedata: InvoiceFormData) => void;
     updateInvoice: (invoiceId: string, invoiceData: Partial<Invoice>) => void;
     removeInvoice: (invoiceId: string) => void;
     removeBulkInvoices: (invoiceIds: string[]) => void;
 }
 
 export const useInvoicesStore = create<InvoicesState>((set, get) => ({
-    // invoices: [],
-    invoices: mockInvoices,
+    invoices: [],
+    // invoices: mockInvoices,
+    async fetchInvoices() {
+        const invoices = await getAllInvoices();
+
+        set(() => ({ invoices }));
+    },
     addInvoice(invoice) {
         set(({ invoices }) => ({ invoices: [...invoices, invoice] }));
     },
