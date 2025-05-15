4e892c01e9e23e12a2470ca3b014d016e2252ec4
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-15 02:36:55 +0200

    page/Customers: added table

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,17 +1,55 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
+import { customerColumns } from '@/components/customers/columns';
+import { DataTable } from '@/components/data-table';
 import { useCustomersStore } from '@/store/customers';
+import { sortByKey } from '@/lib/utils';
+import H1 from '@/components/atoms/H1';
 
 export default function Customers() {
     const customers = useCustomersStore(({ customers }) => customers);
+
+    const sortedCustomers = useMemo(
+        () => sortByKey(customers, 'name'),
+        [customers]
+    );
+
     const fetchCustomers = useCustomersStore(
         ({ fetchCustomers }) => fetchCustomers
+    );
+
+    const removeBulkCustomers = useCustomersStore(
+        ({ removeBulkCustomers }) => removeBulkCustomers
+    );
+
+    const handleRemoveSelected = useCallback(
+        (indexes: number[]) => {
+            const ids = sortedCustomers.reduce((acc: string[], { id }, i) => {
+                if (indexes.includes(i)) {
+                    acc.push(id);
+                }
+                return acc;
+            }, []);
+
+            removeBulkCustomers(ids);
+        },
+        [sortedCustomers, removeBulkCustomers]
     );
 
     useEffect(() => {
         fetchCustomers();
     }, []);
 
-    return <pre>{JSON.stringify(customers, null, 2)}</pre>;
+    return (
+        <>
+            <H1>Customers</H1>
+
+            <DataTable
+                columns={customerColumns}
+                data={sortedCustomers}
+                onRemoveSelected={handleRemoveSelected}
+            />
+        </>
+    );
 }
