707ffcd62f8b5aded3da3b92068a8680bed33b5e
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-24 18:14:37 +0200

    page/customers: removed sorting

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,21 +1,15 @@
 'use client';
 
-import { useCallback, useEffect, useMemo } from 'react';
+import { useCallback, useEffect } from 'react';
 import { toast } from 'sonner';
 import { customerColumns } from '@/components/customers/columns';
 import { DataTable } from '@/components/data-table';
 import { useCustomersStore } from '@/store/customers';
 import H1 from '@/components/atoms/H1';
 import { AddCustomerButton } from '@/components/customers/AddCustomerButton';
-import { sortByKey } from '@/lib/utils';
 
 export default function Customers() {
     const customers = useCustomersStore(({ customers }) => customers);
-
-    const sortedCustomers = useMemo(
-        () => sortByKey(customers, 'name'),
-        [customers]
-    );
 
     const fetchCustomers = useCustomersStore(
         ({ fetchCustomers }) => fetchCustomers
@@ -27,7 +21,7 @@ export default function Customers() {
 
     const handleRemoveSelected = useCallback(
         (indexes: number[]) => {
-            const ids = sortedCustomers.reduce((acc: string[], { id }, i) => {
+            const ids = customers.reduce((acc: string[], { id }, i) => {
                 if (indexes.includes(i)) {
                     acc.push(id);
                 }
@@ -38,7 +32,7 @@ export default function Customers() {
 
             toast.success(`${indexes.length} invoices removed.`);
         },
-        [sortedCustomers, removeBulkCustomers]
+        [customers, removeBulkCustomers]
     );
 
     useEffect(() => {
@@ -55,7 +49,7 @@ export default function Customers() {
 
             <DataTable
                 columns={customerColumns}
-                data={sortedCustomers}
+                data={customers}
                 onRemoveSelected={handleRemoveSelected}
             />
         </>
