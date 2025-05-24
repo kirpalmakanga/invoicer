84b8ccfd8e0a78fce2e1b2b19c97a7de1317a6d8
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-24 18:13:37 +0200

    lib/dates: added formatDate

--- lib/dates.ts
+++ lib/dates.ts
@@ -36,3 +36,63 @@ export function getMonthName(date: string | number | Date) {
 export function getTimestamp(date: string | number | Date) {
     return getDateObject(date).getTime();
 }
+
+type DateFormats =
+    | 'YYYY-MM-DD'
+    | 'DD MMM YYYY'
+    | 'MMMM DD, YYYY'
+    | 'MM/DD/YYYY'
+    | 'DD/MM/YYYY';
+
+export function formatDate(date: string | number | Date, format: DateFormats) {
+    const dateObject = date instanceof Date ? date : new Date(date);
+
+    let result = '⛔️ No valid format';
+
+    switch (format) {
+        case 'YYYY-MM-DD': {
+            result = dateObject
+                .toLocaleDateString('en-GB', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                })
+                .split('/')
+                .reverse()
+                .join('-');
+            break;
+        }
+        case 'DD MMM YYYY': {
+            const formattedDate = dateObject.toLocaleDateString('en-US', {
+                day: '2-digit',
+                month: 'short',
+                year: 'numeric',
+            });
+            const [m, d, y] = formattedDate.split(' ');
+            result = `${d} ${m.toUpperCase()} ${y}`;
+            break;
+        }
+        case 'MMMM DD, YYYY': {
+            result = dateObject.toLocaleDateString(undefined, {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric',
+            });
+            break;
+        }
+        case 'MM/DD/YYYY': {
+            result = dateObject.toLocaleDateString('en-US', {
+                month: '2-digit',
+                day: '2-digit',
+                year: 'numeric',
+            });
+            break;
+        }
+        case 'DD/MM/YYYY': {
+            result = dateObject.toLocaleDateString('en-GB');
+            break;
+        }
+    }
+
+    return result;
+}
